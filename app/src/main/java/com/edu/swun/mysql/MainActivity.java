83d9d8e99840bf29fa4b3a1b0c791fae97295117
SherxCoder <1660261925@qq.com>
2017-05-02 21:57:07 +0800

    修改了内部实现，可以获取到指定条件的用户信息。

--- app/src/main/java/com/edu/swun/mysql/MainActivity.java
+++ app/src/main/java/com/edu/swun/mysql/MainActivity.java
@@ -17,58 +17,21 @@ import java.sql.SQLException;
 public class MainActivity extends AppCompatActivity {
 
     private  static String dbUrl="jdbc:mysql://58fc13364cf64.gz.cdb.myqcloud.com:18302/swunDictionary";
-    //private  static String dbUrl="jdbc:mysql://changxinpark.123nat.com:3306/park";
     private static String dbDriverClass="com.mysql.jdbc.Driver";
     private  static String dbUserName="root";
-    //private  static String dbUserName="changxin";
     private  static String dbUserPassword="swunbs535";
-    //private  static String dbUserPassword="199605";
     public static ResultSet resultSet=null;
     public static Connection connection;
     private  static PreparedStatement pStatement=null;
     private Button mButton;
     private TextView mTextView;
     private static final String TAG="MainActivity";
-    String sql="select * from swunDictionary.user_register";
-    //String sql="select * from user where phone =18328418262";
+    String sql="select * from swunDictionary.user_register where user_id='610225'";
+
     Activity mActivity=this;
     String info=null;
 
-    public void init() {
-        /*new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Class.forName(dbDriverClass);
-                    Log.d("加载驱动", "加载成功");
-                    connection = DriverManager.getConnection(dbUrl, dbUserName, dbUserPassword);
-
-                    pStatement = connection.prepareStatement(sql);
-                    resultSet = pStatement.executeQuery();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    Log.d(TAG, "类加载失败");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        **/
-        try {
-            Class.forName(dbDriverClass);
-            Log.d("加载驱动", "加载成功");
-            connection = DriverManager.getConnection(dbUrl, dbUserName, dbUserPassword);
-            Log.d("获取连接", "连接成功");
-            pStatement = connection.prepareStatement(sql);
-            resultSet = pStatement.executeQuery();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            Log.d(TAG, "类加载失败");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
+  //关闭数据库连接操作
     public void close() {
 
         if (resultSet != null) {
@@ -105,14 +68,9 @@ public class MainActivity extends AppCompatActivity {
         //init();
         mButton= (Button) findViewById(R.id.login_mysql);
         mTextView= (TextView) findViewById(R.id.user_name);
-
-
-
-
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //init();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -132,11 +90,7 @@ public class MainActivity extends AppCompatActivity {
                                     public void run() {
 
                                         mTextView.setText(info);
-                                        try {
-                                            Thread.sleep(3000);
-                                        } catch (InterruptedException e) {
-                                            e.printStackTrace();
-                                        }
+
                                     }
                                 });
 
